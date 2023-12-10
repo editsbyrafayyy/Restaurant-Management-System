80a8c67461dcc15ce28aa8a0fdcb127686ea41d7
RanaMahad <mahad.farooq@gmail.com>
2023-12-10 14:42:23 +0500

    Menu V3
    hopefully final, added all the menu shit, deleted stuff we were'nt doing. Hopefully going back to main menu and exiting shit isnt as complicated now

--- Order_Processing.cpp
+++ Order_Processing.cpp
@@ -1,185 +1,185 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <vector>
-using namespace std;
-
-const int max_orders = 50; // global var 
-
-struct Orders { // using structures instead of implementing OOP
-    int orderId;
-    string customer_name;
-    string ordered_items;
-    int amount;
-};
-
-Orders orders[max_orders]; // Array to store orders
-int order_nums = 0; // Number of orders 
-
-void CreateOrder() { // function to create the order
-
-    if (order_nums >= max_orders) { // to ensure that the orders don't exceed max 
-        cout << "Max Orders Exceeded" << endl;
-        return;
-    }
-
-    Orders newOrder; //declaring new structure 
-
-    cout << "Enter The Order ID: ";
-    cin >> newOrder.orderId;
-
-    cout << "Enter The Customer Name: ";
-    cin.ignore(); // needed for getline for some reason hmmm.
-    getline(cin, newOrder.customer_name);
-
-    cout << "Enter The Items Ordered: ";
-    getline(cin, newOrder.ordered_items);
-
-    cout << "Enter The Amount: ";
-    cin >> newOrder.amount;
-
-    orders[order_nums] = newOrder;
-    order_nums++; // incrementing order nums
-
-    cout << "Your Order Has Been Successfully Created LESSSGOOOO!" << endl;
-}
-
-void ModifyOrder() {
-    int ID; // for comparing
-    bool found = false;
-
-    cout << "Enter order ID to modify: "; //modify using the order ID
-    cin >> ID;
-
-    for (int i = 0; i < order_nums; i++) { // using linear search to find the order
-        if (orders[i].orderId == ID) {
-            found = true;
-
-            cout << "Enter The New Customer's Name: ";
-            cin.ignore();
-            getline(cin, orders[i].customer_name);
-
-            cout << "Enter The New Item Ordered: ";
-            getline(cin, orders[i].ordered_items);
-
-            cout << "Enter The New Amount: ";
-            cin >> orders[i].amount;
-
-            cout << "Order Has Been Successfully Modified HEHE." << endl;
-            break;
-        }
-    }
-
-    if (found == 0) {
-        cout << "This Order Does Not Exist." << endl;
-    }
-}
-
-void CancelOrder() {
-    int ID;
-    bool found = false;
-
-    cout << "Enter order ID to cancel: ";
-    cin >> ID;
-
-    for (int i = 0; i < order_nums; i++) {
-        if (orders[i].orderId == ID) {
-            found = true;
-
-            for (int j = i; j < order_nums - 1; j++) { // Filling The Empty 
-                orders[j] = orders[j + 1];
-            }
-
-            order_nums--;
-
-            cout << "The Order Has Been Successfully Canceled MIMIMI." << endl;
-            break;
-        }
-    }
-
-    if (found == 0) {
-        cout << "The Order Does Not Exist." << endl;
-    }
-}
-
-void OutputOrders() {
-
-    if (order_nums == 0) { // if no orders exist
-        cout << "No Orders Are Available." << endl;
-        return;
-    }
-
-    ofstream outputFile("orders.txt");
-
-    if (!outputFile.is_open()) {
-        cout << "Error opening file." << endl;
-        return;
-    }
-
-    for (int i = 0; i < order_nums; i++) {
-        outputFile << "Order ID: " << orders[i].orderId << "\n";
-        outputFile << "Customer Name: " << orders[i].customer_name << "\n";
-        outputFile << "Items Ordered: " << orders[i].ordered_items << "\n";
-        outputFile << "Quantity: " << orders[i].amount << "\n\n";
-    }
-
-    outputFile.close();
-
-    cout << "Orders written to file successfully." << endl;
-
-    cout << "These Are All The Orders:" << endl;
-    for (int i = 0; i < order_nums; i++) {
-
-        cout << "----------------------" << endl;
-        cout << "Order ID: " << orders[i].orderId << endl;
-        cout << "Customer Name: " << orders[i].customer_name << endl;
-        cout << "Items Ordered: " << orders[i].ordered_items << endl;
-        cout << "Quantity: " << orders[i].amount << endl;
-        cout << "----------------------" << endl;
-    }
-}
-
-int main() {
-    int option;
-
-    do {
-        cout<<"----------------------" << endl;
-
-        cout<<"1. Create A New Order" << endl;
-        cout<<"2. Modify A Existing Order" << endl;
-        cout<<"3. Cancel An Order" << endl;
-        cout<<"4. Show All The Orders" << endl;
-        cout<<"5. Exit" << endl;
-
-        cout<<"----------------------" << endl;
-
-        cout<<"Enter your option: ";
-        cin>>option;
-
-         switch (option) {
-            case 1:
-                CreateOrder();
-                break;
-            case 2:
-                ModifyOrder();
-                break;
-            case 3:
-                CancelOrder();
-                break;
-            case 4:
-                OutputOrders();
-                break;
-            case 5:
-                cout << "Exiting program." << endl;
-                break;
-            default:
-                cout << "Invalid option. Please try again." << endl;
-                break;
-        }
-
-        cout << "\n";
-    } while (option != 5);
-
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <fstream>
+//#include <sstream>
+//#include <vector>
+//using namespace std;
+//
+//const int max_orders = 50; // global var 
+//
+//struct Orders { // using structures instead of implementing OOP
+//    int orderId;
+//    string customer_name;
+//    string ordered_items;
+//    int amount;
+//};
+//
+//Orders orders[max_orders]; // Array to store orders
+//int order_nums = 0; // Number of orders 
+//
+//void CreateOrder() { // function to create the order
+//
+//    if (order_nums >= max_orders) { // to ensure that the orders don't exceed max 
+//        cout << "Max Orders Exceeded" << endl;
+//        return;
+//    }
+//
+//    Orders newOrder; //declaring new structure 
+//
+//    cout << "Enter The Order ID: ";
+//    cin >> newOrder.orderId;
+//
+//    cout << "Enter The Customer Name: ";
+//    cin.ignore(); // needed for getline for some reason hmmm.
+//    getline(cin, newOrder.customer_name);
+//
+//    cout << "Enter The Items Ordered: ";
+//    getline(cin, newOrder.ordered_items);
+//
+//    cout << "Enter The Amount: ";
+//    cin >> newOrder.amount;
+//
+//    orders[order_nums] = newOrder;
+//    order_nums++; // incrementing order nums
+//
+//    cout << "Your Order Has Been Successfully Created LESSSGOOOO!" << endl;
+//}
+//
+//void ModifyOrder() {
+//    int ID; // for comparing
+//    bool found = false;
+//
+//    cout << "Enter order ID to modify: "; //modify using the order ID
+//    cin >> ID;
+//
+//    for (int i = 0; i < order_nums; i++) { // using linear search to find the order
+//        if (orders[i].orderId == ID) {
+//            found = true;
+//
+//            cout << "Enter The New Customer's Name: ";
+//            cin.ignore();
+//            getline(cin, orders[i].customer_name);
+//
+//            cout << "Enter The New Item Ordered: ";
+//            getline(cin, orders[i].ordered_items);
+//
+//            cout << "Enter The New Amount: ";
+//            cin >> orders[i].amount;
+//
+//            cout << "Order Has Been Successfully Modified HEHE." << endl;
+//            break;
+//        }
+//    }
+//
+//    if (found == 0) {
+//        cout << "This Order Does Not Exist." << endl;
+//    }
+//}
+//
+//void CancelOrder() {
+//    int ID;
+//    bool found = false;
+//
+//    cout << "Enter order ID to cancel: ";
+//    cin >> ID;
+//
+//    for (int i = 0; i < order_nums; i++) {
+//        if (orders[i].orderId == ID) {
+//            found = true;
+//
+//            for (int j = i; j < order_nums - 1; j++) { // Filling The Empty 
+//                orders[j] = orders[j + 1];
+//            }
+//
+//            order_nums--;
+//
+//            cout << "The Order Has Been Successfully Canceled MIMIMI." << endl;
+//            break;
+//        }
+//    }
+//
+//    if (found == 0) {
+//        cout << "The Order Does Not Exist." << endl;
+//    }
+//}
+//
+//void OutputOrders() {
+//
+//    if (order_nums == 0) { // if no orders exist
+//        cout << "No Orders Are Available." << endl;
+//        return;
+//    }
+//
+//    ofstream outputFile("orders.txt");
+//
+//    if (!outputFile.is_open()) {
+//        cout << "Error opening file." << endl;
+//        return;
+//    }
+//
+//    for (int i = 0; i < order_nums; i++) {
+//        outputFile << "Order ID: " << orders[i].orderId << "\n";
+//        outputFile << "Customer Name: " << orders[i].customer_name << "\n";
+//        outputFile << "Items Ordered: " << orders[i].ordered_items << "\n";
+//        outputFile << "Quantity: " << orders[i].amount << "\n\n";
+//    }
+//
+//    outputFile.close();
+//
+//    cout << "Orders written to file successfully." << endl;
+//
+//    cout << "These Are All The Orders:" << endl;
+//    for (int i = 0; i < order_nums; i++) {
+//
+//        cout << "----------------------" << endl;
+//        cout << "Order ID: " << orders[i].orderId << endl;
+//        cout << "Customer Name: " << orders[i].customer_name << endl;
+//        cout << "Items Ordered: " << orders[i].ordered_items << endl;
+//        cout << "Quantity: " << orders[i].amount << endl;
+//        cout << "----------------------" << endl;
+//    }
+//}
+//
+//int main() {
+//    int option;
+//
+//    do {
+//        cout<<"----------------------" << endl;
+//
+//        cout<<"1. Create A New Order" << endl;
+//        cout<<"2. Modify A Existing Order" << endl;
+//        cout<<"3. Cancel An Order" << endl;
+//        cout<<"4. Show All The Orders" << endl;
+//        cout<<"5. Exit" << endl;
+//
+//        cout<<"----------------------" << endl;
+//
+//        cout<<"Enter your option: ";
+//        cin>>option;
+//
+//         switch (option) {
+//            case 1:
+//                CreateOrder();
+//                break;
+//            case 2:
+//                ModifyOrder();
+//                break;
+//            case 3:
+//                CancelOrder();
+//                break;
+//            case 4:
+//                OutputOrders();
+//                break;
+//            case 5:
+//                cout << "Exiting program." << endl;
+//                break;
+//            default:
+//                cout << "Invalid option. Please try again." << endl;
+//                break;
+//        }
+//
+//        cout << "\n";
+//    } while (option != 5);
+//
+//    return 0;
+//}
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 using namespace std;
 
+
 //________________________________CONSTANTS____________________________
 const string filename = "hardees.txt";
 //________________________________FUNCTION CALLS______________________
@@ -21,6 +22,9 @@ private:
 	bool availaiblity;
 	string category;
 public:
+
+	MenuItem() : name(""), price(0.0), description(""), availaiblity(false), category("") {}
+
 	void setName(std::string itemName) {
 		name = itemName;
 	}
@@ -62,28 +66,20 @@ public:
 	}
 
 	string serialize() const {
-		// Will be used in loading and unloading objects from text file
-		return name + "*" + to_string(price) + "*" + description + "*" + (availaiblity ? "1" : "0")
-			+ "*" + category;
+		return name + "*" + std::to_string(price) + "*" + description + "*" + (availaiblity ? "1" : "0") + "*" + category;
 	}
 
-	void deserialize(const string& serialzedstring) {
-		// Breaks down the concatenated data line  
-		istringstream iss(serialzedstring); // treats the input as a stream
-
+	void deserialize(const std::string serializedString) {
+		std::istringstream iss(serializedString);
 		getline(iss, name, '*');
 		iss >> price;
-		iss.ignore(); // Ignore the delimiter and any leading whitespaces
+		iss.ignore();
 		getline(iss, description, '*');
 
-		// Read availability as a string
-		string availaibilityStr;
-		getline(iss, availaibilityStr, '*');
+		std::string availabilityStr;
+		getline(iss, availabilityStr, '*');
+		availaiblity = (availabilityStr == "1");
 
-		// Convert the availability string to a boolean
-		availaiblity = (availaibilityStr == "1");
-
-		// Read the category
 		getline(iss, category, '*');
 	}
 
@@ -127,19 +123,22 @@ vector<MenuItem> loadMenuItemsFromFile(const string& filename) {
 	return items;
 }
 
+vector<MenuItem> loadedMenu; //declaring a global array
 
 int main() {
-
+	vector<MenuItem> loadedMenu = loadMenuItemsFromFile(filename);
 	cout << "Welcome to the resturant management system!" << endl;
 	while (true)
 	{
 		displayOptions(0);
-		int n; cin >> n; cin.ignore();
+		int n;
+		cin >> n;
+		cin.ignore();
 		if (n == 1)
 		{
 			manageMenu();
 		}
-		else if (n == 10)
+		else if (n == 0)
 		{
 			system("cls");
 			cout << "Thanks for using our application!" << endl;
@@ -154,15 +153,11 @@ void displayOptions(int a) {
 	if (a == 0) // Main Menu options
 	{
 		cout << "1) Manage Resturant Menu" << endl;
-		cout << "2) Process Orders" << endl;
-		cout << "3) Manage Inventory" << endl;
-		cout << "4) Manage Employees" << endl;
-		cout << "5) Manage your Reservation system" << endl;
-		cout << "6) Generate Invoice" << endl;
-		cout << "7) Generate Reports" << endl;
-		cout << "8) Manage Tables" << endl;
-		cout << "9) Customer Feedback" << endl;
-		cout << "10) Exit Program" << endl;
+		cout << "2) Process Orders and Generate Invoice" << endl;
+		cout << "3) Manage Employees" << endl;
+		cout << "4) Manage Tables/ Reservation system" << endl;
+		cout << "5) Customer Feedback" << endl;
+		cout << "0) Exit Program" << endl;
 	}
 	else if (a == 1)// Managing Resturant Menu options
 	{
@@ -170,99 +165,213 @@ void displayOptions(int a) {
 		cout << "2) Add New Item to Menu" << endl;
 		cout << "3) Remove an Item from Menu" << endl;
 		cout << "4) Modidfy Item Details" << endl;
-		cout << "5) Modify Availaibility" << endl;
-		cout << "6) Modify Discounts" << endl;
-		cout << "7) Exit Menu Managing and go back to Main Menu" << endl;
+		cout << "5) Exit Menu Managing and go back to Main Menu" << endl;
+	}
+	else if (a == 2) //Modifying Item details (subset of Managing Menu)
+	{
+		cout << "1) Change Name" << endl;
+		cout << "2) Change Price" << endl;
+		cout << "3) Change Description" << endl;
+		cout << "4) Change Availaibility" << endl;
+		cout << "5) Change Category" << endl;
 	}
 }
 
 void manageMenu() {
-	system("cls");
-	cout << "\t---Manage your Returant menu---" << endl;
-	displayOptions(1);
-	cout << "Enter your option (number): " << endl;
-	int option; cin >> option; // Provide, with and make them select their option
-	cin.ignore();
-
-	if (option == 1) // DISPLAY CURRENT MENU
+	while (true)
 	{
-		vector<MenuItem> loadedMenu = loadMenuItemsFromFile(filename);
-		for (const auto& item : loadedMenu) {
-			cout << "Item Name: " << item.getName() << endl;
-			cout << "Category: " << item.getCategory() << endl;
-			cout << "Price: $" << item.getPrice() << endl;
-			cout << "Description: " << item.getDescription() << endl;
-			cout << "Availability: " << (item.getAvailaibilty() ? "Available" : "Not Available") << endl;
-			cout << "-----------------\n";
-		}
-	}
-	else if (option == 2)
-	{
-		string newItemName, newItemDescription, newItemCategory;
-		double newItemPrice;
 
-		cout << "Enter details for the new menu item:\n";
-		cout << "Name: ";
-		getline(cin, newItemName);
-		cout << "Description: ";
-		getline(cin, newItemDescription);
-		cout << "Category: ";
-		getline(cin, newItemCategory);
-		cout << "Price: ";
-		cin >> newItemPrice;
-
-		// Create a new MenuItem
-		MenuItem newItem;
-		newItem.setName(newItemName);
-		newItem.setDescription(newItemDescription);
-		newItem.setCategory(newItemCategory);
-		newItem.setPrice(newItemPrice);
-		newItem.setAvailaibilty(true); // Assuming the new item is available
-
-		// Load menu items from file
-		vector<MenuItem> menuItems = loadMenuItemsFromFile(filename);
-
-		// Add the new item to the vector
-		menuItems.push_back(newItem);
-
-		// Save the updated menu items to the file
-		saveMenuItemsToFile(menuItems, filename);
-
-		cout << "New item added and saved to the menu file.\n";
-	}
-	else if (option == 3)//     REMOVE AN ITEM FROM MENU PERMENANTLY
-	{
 		system("cls");
-		cout << "WARNING: Items deletion is permenant" << endl;
-		cout << "Enter 'y' to load item list or anyother key to skip ahead: ";
-		char y; cin >> y;
+		cout << "\t---Manage your Returant menu---" << endl;
+		displayOptions(1);
+		cout << "Enter your option (number): " << endl;
+		int option; cin >> option; // Provide, with and make them select their option
 		cin.ignore();
-		vector<MenuItem> loadedMenu = loadMenuItemsFromFile(filename);
-
-		if (y == 'y' || y == 'Y')
+		loadedMenu = loadMenuItemsFromFile(filename);
+		if (option == 1) // DISPLAY CURRENT MENU
 		{
-			int n = 0;
-			for (const auto& item : loadedMenu)
+			for (const auto& item : loadedMenu) {
+				cout << "Item Name: " << item.getName() << endl;
+				cout << "Category: " << item.getCategory() << endl;
+				cout << "Price: $" << item.getPrice() << endl;
+				cout << "Description: " << item.getDescription() << endl;
+				cout << "Availability: " << (item.getAvailaibilty() ? "Available" : "Not Available") << endl;
+				cout << "-----------------\n";
+			}
+			int n; cout << endl << "Enter 0 to go back to Main Menu or anyother number to Manage Resturant Menu: ";
+			cin >> n;
+			if (n == 0)
 			{
-				cout << n++ << ")" << item.getName() << endl;
+				break;
 			}
 		}
-		string delitem;
-		cout << "Please enter the name of the Item you want to delete: " << endl;
-		getline(cin, delitem);
-
-		auto it = remove_if(loadedMenu.begin(), loadedMenu.end(), [delitem](const MenuItem item)
-			{return (delitem == item.getName()) ? true : false; }); //i'm so surprised i wrote this myself lmao
-		if (it == loadedMenu.end())
+		else if (option == 2) // ENTER NEW ITEM
 		{
-			cout << "Item " << delitem << " Not found :(" << endl;
+			string newItemName, newItemDescription, newItemCategory;
+			double newItemPrice;
+
+			cout << "Enter details for the new menu item:\n";
+			cout << "Name: ";
+			getline(cin, newItemName);
+			cout << "Description: ";
+			getline(cin, newItemDescription);
+			cout << "Category: ";
+			getline(cin, newItemCategory);
+			cout << "Price: ";
+			cin >> newItemPrice;
+
+			// Create a new MenuItem
+			MenuItem newItem;
+			newItem.setName(newItemName);
+			newItem.setDescription(newItemDescription);
+			newItem.setCategory(newItemCategory);
+			newItem.setPrice(newItemPrice);
+			newItem.setAvailaibilty(true); // Assuming the new item is available
+
+			// Load menu items from file
+			vector<MenuItem> menuItems = loadMenuItemsFromFile(filename);
+
+			// Add the new item to the vector
+			menuItems.push_back(newItem);
+
+			// Save the updated menu items to the file
+			saveMenuItemsToFile(menuItems, filename);
+
+			cout << "New item added and saved to the menu file.\n";
+
+			int n;
+			cout << endl << "Enter 0 to go back to Main Menu or anyother number to Manage Resturant Menu: ";
+			cin >> n;
+			if (n == 0)
+			{
+				break;
+			}
+		}
+		else if (option == 3)//     REMOVE AN ITEM FROM MENU PERMENANTLY
+		{
+			system("cls");
+			cout << "WARNING: Items deletion is permenant" << endl;
+			cout << "Enter 'y' to load item list or anyother key to skip ahead: ";
+			char y; cin >> y;
+			cin.ignore();
+			vector<MenuItem> loadedMenu = loadMenuItemsFromFile(filename);
+
+			if (y == 'y' || y == 'Y')
+			{
+				int n = 0;
+				for (const auto& item : loadedMenu)
+				{
+					cout << n++ << ")" << item.getName() << endl;
+				}
+			}
+			string delitem;
+			cout << "Please enter the name of the Item you want to delete: " << endl;
+			getline(cin, delitem);
+
+			auto it = remove_if(loadedMenu.begin(), loadedMenu.end(), [delitem](const MenuItem item)
+				{return (delitem == item.getName()) ? true : false; }); //i'm so surprised i wrote this myself lmao
+			if (it == loadedMenu.end())
+			{
+				cout << "Item " << delitem << " Not found :(" << endl;
+			}
+			else
+			{
+				loadedMenu.erase(it, loadedMenu.end());
+				cout << "Item Removed Succesfully! :)" << endl;
+			}
+
+			saveMenuItemsToFile(loadedMenu, filename);
+			int n; cout << endl << "Enter 0 to go back to Main Menu or anyother number to Manage Resturant Menu: ";
+			cin >> n;
+			if (n == 0)
+			{
+				break;
+			}
+		}
+		else if (option == 4) {
+			vector<MenuItem> loadedMenu = loadMenuItemsFromFile(filename);
+			system("cls");
+			int index = 0; bool found = false;
+
+			for (const auto& item : loadedMenu)
+			{
+				cout << ++index << ") " << item.getName() << endl;
+			}
+
+
+			cout << "Enter the number of the item you want to modify: " << endl;
+			int ItemNum; cin >> ItemNum; cin.ignore();
+
+			if (ItemNum >= 0 && ItemNum <= index)
+			{
+				found = true;
+			}
+
+			if (found)
+			{
+				index = ItemNum - 1;
+				cout << "Item found!\n" << "What do you want to modify about this item?\n";
+				displayOptions(2);
+				int n; cout << "Enter your option by it's corresponding number: "; cin >> n; cin.ignore();
+
+				if (n == 1)
+				{
+					cout << "Enter new name: ";
+					string newname; cin >> newname; cin.ignore();
+					loadedMenu[index].setName(newname);
+				}
+				else if (n == 2)
+				{
+					cout << "Enter new Price: ";
+					double newprice; cin >> newprice;
+					loadedMenu[index].setPrice(newprice);
+				}
+				else if (n == 3)
+				{
+					cout << "Enter new Description: ";
+					string newdesc; cin >> newdesc;
+					loadedMenu[index].setDescription(newdesc);
+
+				}
+				else if (n == 4)
+				{
+					cout << "Enter 0 for not availaible and 1 for avaiaible";
+					bool aval; cin >> aval;
+					if (aval == 0 || aval == 1)
+					{
+						loadedMenu[index].setAvailaibilty(aval);
+					}
+					else
+						cout << "Error: Enter either 0 or 1!";
+				}
+				else if (n == 5)
+				{
+					cout << "Enter new Category: ";
+					string newcat; cin >> newcat; cin.ignore();
+					loadedMenu[index].setCategory(newcat);
+				}
+				saveMenuItemsToFile(loadedMenu, filename);
+			}
+			else
+			{
+				cout << "Sorry the item name you entered was not found!" << endl
+					<< "make sure spellings are correct, search is also case sensitive";
+			}
+			int n; cout << endl << "Enter 0 to go back to Main Menu or anyother number to Manage Resturant Menu: ";
+			cin >> n;
+			if (n == 0)
+			{
+				break;
+			}
+		}
+		else if (option == 5) {
+			system("cls");
+			cout << "Exited Menu Mangement Succesfully!" << endl;
+			break;
 		}
 		else
 		{
-			loadedMenu.erase(it, loadedMenu.end());
-			cout << "Item Removed Succesfully! :)" << endl;
+			cout << "Error: Please choose from 1-5." << endl;
 		}
-
-		saveMenuItemsToFile(loadedMenu, filename);
 	}
 }
